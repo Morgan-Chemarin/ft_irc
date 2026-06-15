026427fe23622374dadb6f85611a26ee97cf7e48
Morgan Chemarin <mchemari@z4r4p6.42lyon.fr>
2026-06-16 01:08:30 +0200

    feat: IRC MESSAGE ERROR/ add security

--- src/commands/CommandJoin.cpp
+++ src/commands/CommandJoin.cpp
@@ -6,33 +6,69 @@ void CommandJoin::execute(Server& server, Client& client, const IRCPrompt& promp
 {
 	if (prompt.args.empty()) 
 	{
-        std::cout << "ERR_NEEDMOREPARAMS pour le fd " << client.getFd() << "\n";
-        return;
-    }
+		server.sendMessage(client.getFd(), MessageBuilder("461")
+			.setPrefix("ircserv")
+			.setParam(client.getNickname())
+			.setParam("JOIN")
+			.setContent("Not enough parameters"));
+		return;
+	}
 
 	// le premier arguement ou ya le nom du channel peut etre composé de plueirus channel 
 	// ex = JOIN room1,room2...
-	//? pour linstant je gere que un channel en arg je ferai demain
+	//! pour linstant je gere que un channel en arg je ferai demain
 	std::string channelName = prompt.args[0];
+
+	std::cout << channelName << std::endl;
+	// check le nom du channel doit commencer par un caractere speciale/pas despace
+	if (channelName.empty() || (channelName[0] != '#' && channelName[0] != '&' && channelName[0] != '+' && channelName[0] != '!'))
+	{
+		server.sendMessage(client.getFd(), MessageBuilder("476")
+			.setPrefix("ircserv")
+			.setParam(client.getNickname())
+			.setParam(channelName)
+			.setContent("Bad Channel Mask"));
+		return;
+	}
 
 	Channel* chan = server.getChannel(channelName);
 	// si le channel n'existe pas on le créé, càd on créé une nouvelle instance 
 	// et on lajoute a la map des channels de Server a la key = nom du channel
-	if (chan == NULL) {
-		server.addChannel(channelName); // Plus de accès direct à _channels !
-		chan = server.getChannel(channelName); 
-
-		//? TEST
-		std::cout << "Le client a cree la room " << channelName << std::endl;
+	if (chan == NULL)
+	{
+		server.addChannel(channelName);
+		chan = server.getChannel(channelName);
+		std::cout << "Le client a cree la room " << channelName << std::endl; // ?test
 	}
 
-	//! check si le fd nest pas deja dans le channel
+	// check si le fd nest pas deja dans le channel
+	if (chan->hasMember(client.getFd()))
+		return;
 
 	// on recupere le pointeur du client pour lajouter dans le channel ( que lon vient de creer ou non )
 	chan->addMember(&client);
-
 	std::cout << "Le client " << client.getNickname() << " (fd " << client.getFd() 
-              << ") a rejoint le channel " << channelName << std::endl;
+			  << ") a rejoint le channel " << channelName << std::endl;
+
+	// broadcast pour envoyer a tous les membres du channel linf oque quelquun a rejoint
+	// :Momo!user@host JOIN #channel
+	std::string userPrefix = client.getNickname() + "!" + client.getUsername() + "@localhost";
+    const std::map<int, Client*>& members = chan->getMembers();
+	// on droadcats a TOUT le monde, celui qui rejoint aussi
+	for (std::map<int, Client*>::const_iterator it = members.begin(); it != members.end(); ++it)
+    {
+        server.sendMessage(it->first, MessageBuilder("JOIN")
+            .setPrefix(userPrefix)
+            .setContent(channelName));
+    }
+
+	// envoyer le topic du channel sil en a un ( //! 331 sil en a pas mais pour linstant pn fait comme on peut)
+	server.sendMessage(client.getFd(), MessageBuilder("332")
+        .setPrefix("ircserv")
+        .setParam(client.getNickname())
+        .setParam(channelName)
+        .setContent("Bienvenue sur " + channelName));
+
 
 	//? envoyer les messages IRC avec send
 	// RPL_JOIN, RPL_NAMREPLY, ERR_NEEDMOREPARAMS
